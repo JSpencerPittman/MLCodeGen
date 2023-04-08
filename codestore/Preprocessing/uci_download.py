6c0e1c332531a635d69910d6fd6d429108188230
Jason Pittman <jspencerpittman@gmail.com>
2023-04-07 19:27:01 -0500

    Multifile download option

--- codestore/Preprocessing/uci_download.py
+++ codestore/Preprocessing/uci_download.py
@@ -1,5 +1,6 @@
 import os
 from urllib.request import urlretrieve
+from typing import List
 
 DATASET_DIR = "datasets"
 DATASET_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/"
@@ -14,3 +15,10 @@ def download_dataset(file_name:str) -> None:
     file_url  = DATASET_URL + '/' + file_name
        
     urlretrieve(file_url, file_path)
+
+
+# Install Multiple Files
+def download_dataset(file_names:List[str]) -> None:    
+    for i, file_name in file_names:
+        print(f"Downloading: {(i+1)}/{len(file_names)} {file_name}")
+        download_dataset(file_name)
